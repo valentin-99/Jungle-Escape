00dc19a07b0487e05a65cb00db5da4055ac151b6
valentin-99 <61514366+valentin-99@users.noreply.github.com>
2022-03-17 23:59:03 +0200

    update

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform pos1, pos2;
+    public float speed;
+    public Transform startPos;
+    Vector3 nextPos;
+
     void Start()
     {
-        
+        nextPos = startPos.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // daca platforma a ajuns la pos1, urmatoarul target va fi pos2 si invers.
+        if (transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+
+        if (transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+
+        // MoveTowards ia pozitia curenta si se misca catre target (nextPos)
+        // la viteza speed * Time.deltaTime
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
     }
 }
